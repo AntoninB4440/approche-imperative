65273037e94a6cd61740475a09c3a72e1fdf8460
AntoninB4440 <barceloantonin@gmail.com>
2021-01-22 15:53:43 +0100

    v1

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,24 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 5;
+		short s = -15500;
+		int i = 3549870;
+		long l = 100000000L;
+		float f = 2.69f ;
+		double d = 38.89D;
+		char c = 'c';
+		boolean myBool = true;
+		
+		System.out.println("Byte : " + b);
+		System.out.println("Boolean : " + myBool);
+		
+		String randomString ="Voici le résultat d'un calcul: \n1+5 = 6";
+		System.out.println(randomString);
+		 
+	}
+
+}
