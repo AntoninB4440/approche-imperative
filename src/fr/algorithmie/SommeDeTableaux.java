dd8fb31934676be37e819dd9c5fcb563c64ae67b
AntoninB4440 <barceloantonin@gmail.com>
2021-01-25 15:23:55 +0100

    SommeDeTableaux fonctionnel

---
+++ src/fr/algorithmie/SommeDeTableaux.java
@@ -0,0 +1,26 @@
+package fr.algorithmie;
+
+public class SommeDeTableaux {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] array1 = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		int[] array2 = {-1, 12, 17, 14, 5, -9, 0, 18, -6, 0, 4, -13, 5, 7, -2, 8, -1} ;
+		
+		int[] arraySum = new int[ (int) Math.max(array1.length, array2.length)];
+		
+		
+		System.out.println("Affichage de la somme des deux tableaux");
+		for (int i = 0; i < arraySum.length; i++) {
+			if (array1.length > i) {
+				arraySum[i] += array1[i]; 
+			}
+			if (array2.length > i) {
+				arraySum[i] += array2[i]; 
+			}
+		System.out.print(arraySum[i] + " ");
+		}
+	}
+
+}
