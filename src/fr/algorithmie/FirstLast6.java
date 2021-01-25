fac8bf6ea186c0d32075197c1a2d41611f65cde0
AntoninB4440 <barceloantonin@gmail.com>
2021-01-25 16:02:09 +0100

    FirstLast6 update

--- src/fr/algorithmie/FirstLast6.java
+++ src/fr/algorithmie/FirstLast6.java
@@ -5,14 +5,12 @@ public class FirstLast6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] array = {6,1,2,5,6};
+		int[] array = {1,2,5};
 		boolean verif = false;
 		
 		if (array.length !=0 && (array[0] == 6 || array[array.length -1] == 6)){
 			verif = true;
-		} else {
-			verif = false;
-		}
+		};
 		
 		System.out.println("Est-ce que array est un FirstLast6 ? : " + verif);
 	}
