79ac4cb58deebe3cccde0aceab89832b355d1141
AntoninB4440 <barceloantonin@gmail.com>
2021-01-25 14:35:22 +0100

    CalculMoyenne fonctionnel

---
+++ src/fr/algorithmie/CalculMoyenne.java
@@ -0,0 +1,20 @@
+package fr.algorithmie;
+
+public class CalculMoyenne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+		 
+		 double sum = 0;
+		 double moyenne;		 
+		 for (int i = 0; i < array.length; i++) {
+			sum = sum + array[i];
+		 }
+		 
+		 moyenne = sum/array.length;
+		 
+		 System.out.println("La moyenne de l'array est : " + moyenne);
+	}
+
+}
